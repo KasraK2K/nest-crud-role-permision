05e420d1dd976bc880b2852405d3b47dfb11dd99
Kasra_K2K <Kasra_K2K@yahoo.com>
2021-09-24 20:51:00 +0330

    add schema for response dto

--- src/company/dto/response/create-res.dto.ts
+++ src/company/dto/response/create-res.dto.ts
@@ -1,6 +1,19 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
+@Exclude()
 export class CreateResDto {
-  @Exclude()
-  id: number;
+  @Expose()
+  name: string;
+
+  @Expose()
+  created_at: Date;
+
+  @Expose()
+  updated_at: Date;
+
+  @Expose()
+  deleted_at: Date;
+
+  @Expose()
+  version: number;
 }
